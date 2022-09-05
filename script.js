2c1010c1ffcf1dcc78e94d1264008069a6ecc5f1
Manuel Estrada <manuel.estrada16@gmail.com>
2022-09-05 13:59:28 -0400

    finished level designs, added opened and closed doors

--- script.js
+++ script.js
@@ -26,12 +26,22 @@ loadSprite('bottom-right-wall', "assets/bottom-right-wall.png")
 loadSprite('bottom-left-wall', "assets/bottom-left-wall.png")
 
 loadSprite('top-closed-door', "assets/top-closed-door.png")
+loadSprite('top-opened-door', "assets/top-opened-door.png")
 loadSprite('bottom-closed-door', "assets/bottom-closed-door.png")
+loadSprite('bottom-opened-door', "assets/bottom-opened-door.png")
+loadSprite('right-closed-door', "assets/right-closed-door.png")
+loadSprite('right-opened-door', "assets/right-opened-door.png")
+loadSprite('left-closed-door', "assets/left-closed-door.png")
+loadSprite('left-opened-door', "assets/left-opened-door.png")
 
 loadSprite('block', "assets/block.png")
+loadSprite('switch', "assets/switch.png")
+loadSprite('switch-pressed', "assets/switch-pressed.png")
 
 loadSprite('enemy-1', "assets/enemy-1.png")
 loadSprite('enemy-2', "assets/enemy-2.png")
+loadSprite('boss-1', "assets/boss-1.png")
+loadSprite('boss-2', "assets/boss-2.png")
 loadSprite('final-boss', "assets/final-boss.png")
 
 //test code to add sprite on the page
@@ -50,12 +60,12 @@ add([
 scene("game", () => {
     //define the layout of the floor using symbols defined 
     const mapLayout1 = [
-        '1tttttttttt2',
+        '1ttttt5tttt2',
         'l          r',
         'l          r',
         'l          r',
         'l          r',
-        'l          r',
+        '0          @',
         'l          r',
         'l          r',
         'l          r',
@@ -67,8 +77,8 @@ scene("game", () => {
         'l         x r',
         'l     x   x r',
         'lxx   x   x r',
-        'l x   x   x r',
-        'l x e x f x r',
+        '9 x   x   x r',
+        'l x e x f x @',
         'l x   x   x r',
         'l x   x   x r',
         'l     x     r',
@@ -77,45 +87,45 @@ scene("game", () => {
 
     const mapLayout3 = [
         '1tttttttttt2',
-        'l xx   xxx r',
-        'l x  x   x r',
-        'lx   x    xr',
-        'l    x     r',
-        'lx   x    xr',
-        'l xxx    x r',
-        'l xxx xxx  r',
+        'l xx    xx r',
+        'l x      x r',
+        'lx        xr',
+        'l    g     @',
         'l          r',
+        'lx        xr',
+        'l x      x r',
+        'l xx    xx r',
         '3bbbbbbbbbb4',
     ]
 
     const mapLayout4 = [
         '1tttttttttt2',
         'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
+        'l      f   r',
+        'l x      xxr',
+        '0 x   x x  !',
+        'lx  e x x xr',
+        'l     x x  r',
+        'l e   x  f r',
+        'l      x   r',
         '3bbbbbbbbbb4',
     ]
 
     const mapLayout5 = [
         '1tttttttttt2',
+        'l xx    xx r',
+        'l x      x r',
+        'lx        xr',
+        '0    h     r',
         'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
+        'lx        xr',
+        'l x      x r',
+        'l xx    xx r',
         '3bbbbbbbbbb4',
     ]
 
     const mapLayout6 = [
-        '1tttttttttt2',
+        '1tttt8ttttt2',
         'lxxxx  xxxxr',
         'l          r',
         'lxxxxxxxxx r',
@@ -124,20 +134,20 @@ scene("game", () => {
         'l          r',
         'lxxxx  xxxxr',
         'l          r',
-        '3bbbbbbbbbb4',
+        '3bbbb8bbbbb4',
     ]
 
     const mapLayout7 = [
         '1tttttttttt2',
+        'l xx    xx r',
+        'l x      x r',
+        'lx   i    xr',
         'l          r',
         'l          r',
-        'l    g     r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        'l          r',
-        '3bbbbbbbbbb4',
+        'lx        xr',
+        'l x      x r',
+        'l xx    xx r',
+        '3bbbb8bbbbb4',
     ]
 
     //defines the symbols to be used for the map layout, each symbol is assigned a sprite that was loaded above using loadSprite(), also defines width and height of the map
@@ -153,15 +163,26 @@ scene("game", () => {
         '3': () => [sprite('bottom-left-wall')],
         '4': () => [sprite('bottom-right-wall')],
 
+        '5': () => [sprite('top-closed-door')],
+        '6': () => [sprite('top-opened-door')],
+        '7': () => [sprite('bottom-closed-door')],
+        '8': () => [sprite('bottom-opened-door')],
+        '9': () => [sprite('left-closed-door')],
+        '0': () => [sprite('left-opened-door')],
+        '!': () => [sprite('right-closed-door')],
+        '@': () => [sprite('right-opened-door')],
+
         'x': () => [sprite('block')],
         'e': () => [sprite('enemy-1')],
         'f': () => [sprite('enemy-2')],
-        'g': () => [sprite('final-boss')]
+        'g': () => [sprite('boss-1')],
+        'h': () => [sprite('boss-2')],
+        'i': () => [sprite('final-boss')],
     }
 
     //addLevel(mapLayout1, levelSettings)
     //addLevel(mapLayout2, levelSettings)
-    addLevel(mapLayout7, levelSettings)
+    addLevel(mapLayout1, levelSettings)
 })
 
 go("game")
